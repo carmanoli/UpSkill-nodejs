a5f03b409653545c17cdec1f92a83409012689ad
JimmyCanete <jimmycanete1@gmail.com>
2022-10-08 15:34:26 +0100

    resolução do bug de pesquisar autor pelo id

--- pessoa.js
+++ pessoa.js
@@ -19,26 +19,17 @@ app.get('/realizadores', async function (req, res) {
     res.json(atores);
 });
 
-//Pesquisar um ator pelo ID [BUG]
-async function pesquisarAtor(id) {
-    app.get('/atores/:id_ator', async function (req, res) {
+//Pesquisar um ator pelo ID
+    app.get('/atores/:idAutor', async function (req, res) {
         console.log(connection.state);
-
         // Procurar ator com o mesmo ID
-        let atores = await queryDB("SELECT pessoa.nome, pessoa.datanascimento, pessoa.genero  FROM `ator`, `pessoa` WHERE ator.idpessoa = pessoa.idpessoa AND pessoa.idpessoa = ?", [id]);
-        if (atores.length > 0){
+        let atores = await queryDB("SELECT pessoa.nome, pessoa.datanascimento, pessoa.genero  FROM `ator`, `pessoa` WHERE ator.idpessoa = pessoa.idpessoa AND pessoa.idpessoa = ?", [req.params.idAutor]);
+        if (atores.length > 0) {
+            console.log(req.params.idAutor)
             res.json(atores);
-        }else{
-            // Autor não existe
-            throw `Não existe um autor com esse ID`
         }
     })
-}
-try{
-    pesquisarAtor(14)
-}catch (e){
-    console.log('nok', e)
-}
+
 
 //Pesquisar um realizador pelo o nome ou ID
 //Adicionar um ator
