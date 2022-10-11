db906706de2e90cd3a4149bf607f3f06f19cb6c6
António Nobre <108399196+antonionobre@users.noreply.github.com>
2022-10-11 09:09:02 +0100

    Update utilizador.js

--- utilizador.js
+++ utilizador.js
@@ -1,15 +1,30 @@
-const {queryDB, connection} = require("./connection");
-
 const express = require('express');
 const app = express();
+const {queryDB} = require("./connection.js");
+const {connection} = require("./connection");
 
-const router = express.Router();
+// listar utilizadores existentes
+app.listen(3000);
+app.get('/utilizador', async function (req, res) {
+    let utilizador = await queryDB("SELECT * FROM utilizador");
+    res.json(utilizador);
+})
 
-router.get('/', async function (req, res) {
-    console.log(connection.state);
-    let utilizador = await queryDB(`SELECT * FROM utilizador`, [])
+// listar visualizações de utilizadores
+app.get('/visualizacao', async function (req, res) {
+    let visualizacao = await queryDB("SELECT * FROM visualizacao");
+    res.json(visualizacao);
+})
+// listar pontuação de utilizadores
+app.get('/pontuacao', async function (req, res) {
+    let pontuacao = await queryDB("SELECT * FROM pontuacao");
+    res.json(pontuacao);
+})
 
-    res.send(utilizador);
-});
+// listar a lista de utilizador
+app.get('/utilizador/lista', async function (req, res) {
+    let utilizadorlista = await queryDB("SELECT * FROM utilizador, pontuacao, visualizacao WHERE utilizador.idutilizador = pontuacao.idutilizador AND utilizador.idutilizador = visualizacao.idutilizador");
+    res.json(utilizadorlista);
+})
 
 module.exports = router;
